2c038b5f09c2746fdb309a13e9f759e8e2a78eb3
Vasil Laba <vasillaba1@gmail.com>
2022-10-06 22:56:55 +0200

    Initial commit

---
+++ src/main/java/com/calcuator/supercalculatorweb/SuperCalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.calcuator.supercalculatorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCalculatorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCalculatorWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calcuator/supercalculatorweb/SuperCalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcuator.supercalculatorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCalculatorWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

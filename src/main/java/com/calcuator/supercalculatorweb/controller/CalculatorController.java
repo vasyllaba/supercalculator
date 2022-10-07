a472cf930c48570f751288d586d4c546c89b5e6f
Vasil Laba <vasillaba1@gmail.com>
2022-10-07 17:02:32 +0200

    Add index and get all contorllers

--- src/main/java/com/calcuator/supercalculatorweb/controller/CalculatorController.java
+++ src/main/java/com/calcuator/supercalculatorweb/controller/CalculatorController.java
@@ -1,7 +1,12 @@
 package com.calcuator.supercalculatorweb.controller;
 
+import com.calcuator.supercalculatorweb.model.Calculator;
 import com.calcuator.supercalculatorweb.service.CalculatorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CalculatorController {
@@ -9,5 +14,17 @@ public class CalculatorController {
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/")
+    public String index(Map<String, Object> model) {
+        List<Calculator> goals = calculatorService.getAll();
+        return "index";
+    }
+
+    @GetMapping("/calculation")
+    public String list(Map<String, Object> model) {
+        model.put("calculations", calculatorService.getAll());
+        return "calculation";
     }
 }

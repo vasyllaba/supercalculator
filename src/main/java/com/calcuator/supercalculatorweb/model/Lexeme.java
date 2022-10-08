f9239b8f8872526552d03cf953d58eb494d165c4
Vasil Laba <vasillaba1@gmail.com>
2022-10-08 11:47:46 +0200

    Add lex analyze method

--- src/main/java/com/calcuator/supercalculatorweb/model/Lexeme.java
+++ src/main/java/com/calcuator/supercalculatorweb/model/Lexeme.java
@@ -2,6 +2,9 @@ package com.calcuator.supercalculatorweb.model;
 
 import com.calcuator.supercalculatorweb.model.enums.LexemeType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexeme {
     LexemeType type;
     String value;
@@ -14,5 +17,67 @@ public class Lexeme {
     public Lexeme(LexemeType type, Character value) {
         this.type = type;
         this.value = value.toString();
+    }
+
+    public static List<Lexeme> lexAnalyze(String expText) {
+        ArrayList<Lexeme> lexemes = new ArrayList<>();
+        int pos = 0;
+        while (pos < expText.length()) {
+            char c = expText.charAt(pos);
+            switch (c) {
+                case '(':
+                    lexemes.add(new Lexeme(LexemeType.LEFT_BRACKET, c));
+                    pos++;
+                    continue;
+                case ')':
+                    lexemes.add(new Lexeme(LexemeType.RIGHT_BRACKET, c));
+                    pos++;
+                    continue;
+                case '+':
+                    lexemes.add(new Lexeme(LexemeType.OP_PLUS, c));
+                    pos++;
+                    continue;
+                case '-':
+                    lexemes.add(new Lexeme(LexemeType.OP_MINUS, c));
+                    pos++;
+                    continue;
+                case '*':
+                    lexemes.add(new Lexeme(LexemeType.OP_MUL, c));
+                    pos++;
+                    continue;
+                case '/':
+                    lexemes.add(new Lexeme(LexemeType.OP_DIV, c));
+                    pos++;
+                    continue;
+                default:
+                    if (c <= '9' && c >= '0') {
+                        StringBuilder sb = new StringBuilder();
+                        do {
+                            sb.append(c);
+                            pos++;
+                            if (pos >= expText.length()) {
+                                break;
+                            }
+                            c = expText.charAt(pos);
+                        } while (c <= '9' && c >= '0');
+                        lexemes.add(new Lexeme(LexemeType.NUMBER, sb.toString()));
+                    } else {
+                        if (c != ' ') {
+                            throw new RuntimeException("Unexpected character: " + c);
+                        }
+                        pos++;
+                    }
+            }
+        }
+        lexemes.add(new Lexeme(LexemeType.EOF, ""));
+        return lexemes;
+    }
+
+    @Override
+    public String toString() {
+        return "Lexeme{" +
+                "type=" + type +
+                ", value='" + value + '\'' +
+                '}';
     }
 }
